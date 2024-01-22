928efd80e7e49f0d354e37ecb515067f808098ad
SoonIter <sooniter@gmail.com>
2024-01-23 01:20:14 +0800

    chore(infra): remove lets_find_up bench cases

--- crates/j2rs_find_up_simple/benches/basic_parent_dir.rs
+++ crates/j2rs_find_up_simple/benches/basic_parent_dir.rs
@@ -6,15 +6,15 @@ fn j2rs_find_up_simple() -> PathBuf {
   j2rs_find_up_simple::find_up("Cargo.lock").unwrap()
 }
 
-fn lets_find_up() -> PathBuf {
-  lets_find_up::find_up("Cargo.lock").unwrap().unwrap()
-}
+// fn lets_find_up() -> PathBuf {
+//   lets_find_up::find_up("Cargo.lock").unwrap().unwrap()
+// }
 
 fn main_bench(b: &mut Criterion) {
   let mut group = b.benchmark_group("basic_parent_dir_benchmark");
   group.sample_size(10);
   group.bench_function("j2rs_find_up_simple", |b| b.iter(j2rs_find_up_simple));
-  group.bench_function("lets_find_up", |b| b.iter(lets_find_up));
+  // group.bench_function("lets_find_up", |b| b.iter(lets_find_up));
 }
 
 criterion_group!(benches, main_bench);
